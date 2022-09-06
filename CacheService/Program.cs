0c8d1b9039b8ea46623e19cf0f7b3af0cdd488e8
VenkateshSrini <venkateshsrini3@gmail.com>
2022-09-06 08:48:07 +0530

    code fixed for kubernetes and PV

--- CacheService/Program.cs
+++ CacheService/Program.cs
@@ -3,7 +3,11 @@ using Net.DistributedFileStoreCache;
 var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.ConfigureKestrel(webOptions =>
 {
-    webOptions.ListenLocalhost(5275, kestrelOptions => {
+    //webOptions.ListenLocalhost(5275, kestrelOptions => {
+    //    kestrelOptions.Protocols = Microsoft.AspNetCore.Server.Kestrel.Core.HttpProtocols.Http2;
+    //});
+    webOptions.ListenAnyIP(5275, kestrelOptions =>
+    {
         kestrelOptions.Protocols = Microsoft.AspNetCore.Server.Kestrel.Core.HttpProtocols.Http2;
     });
 });
